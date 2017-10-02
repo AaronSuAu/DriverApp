ff20f7a42a6d5261bd31727a156f135133c94a74
Aaron <aaroncareer.au@gmail.com>
2017-10-02 11:57:41 +1100

    url constant

--- src/main/java/http/HttpToken.java
+++ src/main/java/http/HttpToken.java
@@ -3,4 +3,5 @@ package http;
 public class HttpToken {
 	public static final String CLIENT_TOKEN = "$2y$10$HnIpEBB9xgFQmUfDYWpvQenYRiBhr336VDRV.DOCd3OWMY8emf3o2";
 	public static final String OFFICER_TOKEN ="o6RdxluHDu7fHBgpk9R2hBMLLfP6z74v9pnopAE0co5EuZflZgw9cXavJ3zL";
+	public static final String ROOT_URL = "http://localhost:8090";
 }
