77f97cd628e0312ac1f56f82a40f256adacf80f6
Aaron <aaroncareer.au@gmail.com>
2017-10-02 11:56:09 +1100

    token value

---
+++ src/main/java/http/HttpToken.java
@@ -0,0 +1,6 @@
+package http;
+
+public class HttpToken {
+	public static final String CLIENT_TOKEN = "$2y$10$HnIpEBB9xgFQmUfDYWpvQenYRiBhr336VDRV.DOCd3OWMY8emf3o2";
+	public static final String OFFICER_TOKEN ="o6RdxluHDu7fHBgpk9R2hBMLLfP6z74v9pnopAE0co5EuZflZgw9cXavJ3zL";
+}
